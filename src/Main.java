4e75316231293caf8f8041c3db268d6ec305b7b2
Muna <mmhussein125@gmail.com>
2019-01-08 17:34:01 +0000

    Imports relevant packages

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
+mport javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;﻿
+
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World!");
     }
 }
