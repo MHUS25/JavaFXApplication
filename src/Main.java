f4e8c15756a3a01cbbf8395664997d994fa341fe
Muna <mmhussein125@gmail.com>
2019-01-09 00:57:43 +0000

    Removes 'illegal' character

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;﻿
+import javafx.stage.Stage;
 
 public class Main extends Application {
 
